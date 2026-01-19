2fe3b1abe14c6fb15fccbeab88351d4a460f6289
Sergi Torres Albert <STORALB@upv.edu.es>
2026-01-19 23:17:55 +0100

    logica de baraja y reparto de cartas

--- server/game/sala.js
+++ server/game/sala.js
@@ -1,10 +1,12 @@
 const constantes = require("./constantes");
 const Jugador = require("./jugador");
+const Mesa = require("./mesa");
+const Baraja = require("./baraja");
 
 class Sala {
     constructor(id, config) {
         this.id = id
-        this.estado = constantes.ESTADOS.LOBBY
+        this.estado = constantes?.ESTADOS?.LOBBY || "LOBBY"
         this.ronda = 0
 
         const limite = config?.maxJugadores || 4;
@@ -16,11 +18,14 @@ class Sala {
 
         // DATOS
         this.jugadores = []
-        this.mesa = mesa
+        this.mesa = new Mesa()
 
         // ESTADO INTERCAMBIO
         this.intercambiosPendientes = []
 
+        this.baraja = null
+
+        this.preferenciaBaraja48 = config?.baraja48 || false;
 
     }
 
@@ -28,6 +33,37 @@ class Sala {
         const nuevoJugador = new Jugador(socketId, nombre);
         this.jugadores.push(nuevoJugador);
         return nuevoJugador;
+    }
+
+
+    iniciar_partida(){
+        if (this.jugadores.length < 4) return {error : "Faltan jugadores"};
+        if (this.estado !== "LOBBY") return { error: "Ya ha empezado" };
+
+        this.estado = "JUGANDO";
+
+        const numJugadores = this.jugadores.length;
+        let usarMazoGrande = false;
+
+        if (numJugadores === 4) {
+            usarMazoGrande = this.preferenciaBaraja48;
+        }else if (numJugadores === 6) {  // 5 ya es = false -> No se cambia
+            usarMazoGrande = true;
+        }
+
+        this.baraja = new Baraja(usarMazoGrande);
+        this.baraja.barajar();
+
+        const totalCartas = this.baraja.cartas.length;
+        const cartasPorJugador = Math.floor(totalCartas / numJugadores);
+
+        this.jugadores.forEach(jugador => {
+            jugador.mano = this.baraja.robar(cartasPorJugador)
+            // ordena cartas
+            jugador.mano.sort((a,b) => a.fuerza - b.fuerza)
+        });
+        
+        return {exito : true}
     }
 }
 
